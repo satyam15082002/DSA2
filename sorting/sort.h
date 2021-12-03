d81d6aa63805b0692afea52ef0bae19b71ea742f
Satyam Tamrakar <63050895+satyam15082002@users.noreply.github.com>
2021-12-03 12:36:00 +0530

    Update sort.h

--- sorting/sort.h
+++ sorting/sort.h
@@ -12,7 +12,7 @@ void bubble_sort(int a[],int n)
     int count=0;
     for(int i=0;i<n-1;i++)
     {
-        for(int j=0;j<n-1;j++)
+        for(int j=0;j<n-i-1;j++)
         {
             count++;
             if(a[j]>a[j+1])
