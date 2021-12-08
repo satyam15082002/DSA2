be823994acd622d698086986b961f1e94df3528e
Satyam Tamrakar <63050895+satyam15082002@users.noreply.github.com>
2021-12-08 12:21:44 +0530

    Added Heap Sort

--- sorting/sort.h
+++ sorting/sort.h
@@ -3,7 +3,9 @@
 void bubble_sort(int a[],int n);
 void selection_sort(int a[],int n);
 void insertion_sort(int a[],int n);
-
+//Heap Sort functions
+void heap_sort(int a[],int n);
+void heapify(int a[],int n,int i);
 
 
 void bubble_sort(int a[],int n)
@@ -65,4 +67,33 @@ void insertion_sort(int a[],int n)
     }
     std::cout<<"Insertion sort count:"<<count<<std::endl;
 
+}
+void heapify(int a[],int n,int i)
+{
+	int largest=i,temp;
+	int l=2*i+1,r=2*i+2;
+	if(l<n&&a[l]>a[i])
+		largest=l;
+	if(r<n&&a[r]>a[largest])
+		largest=r;
+	if(largest!=i)
+	{
+		temp=a[i];
+		a[i]=a[largest];
+		a[largest]=temp;
+		heapify(a,n,largest);
+	}
+}
+void heap_sort(int a[],int n)
+{
+	int i,temp;
+	for(i=n/2-1;i>=0;i--)
+		heapify(a,n,i);
+	for(i=n-1;i>0;i--)
+	{
+		temp=a[0];
+		a[0]=a[i];
+		a[i]=temp;
+		heapify(a,i,0);
+	}
 }
