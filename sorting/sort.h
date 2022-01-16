becf96caa74419ddbac4a722a5c3186ee147fc71
Satyam Tamrakar <63050895+satyam15082002@users.noreply.github.com>
2022-01-16 07:11:54 -0500

    Added merge and quick sort

--- sorting/sort.h
+++ sorting/sort.h
@@ -9,6 +9,13 @@ void heapify(int a[],int n,int i);
 //count sort
 void countSort(int a[],int n);
 
+//Merge Sort
+void merge(int a[],int low ,int mid,int high); 
+void merge_sort(int a[],int low,int high);
+
+//Quick Sort
+int partition(int a[],int low ,int high);
+void quick_sort(int a[],int low, int high);
 
 void bubble_sort(int a[],int n)
 {
@@ -116,4 +123,80 @@ void countSort(int a[],int n)
     for(int i=0;i<n;i++)
         a[i]=output[i];
     delete[] output;
+}
+
+// Merge Sort
+void merge(int a[],int low ,int mid,int high)
+{
+    int* c=new int[high+1];
+    int i=low,j=mid+1,k=0;
+    while((i<=mid)&&(j<=high))
+    {
+        if(a[i]<a[j])
+        {
+            c[k]=a[i];
+            i++; k++;
+        }
+        else
+        {
+            c[k]=a[j];
+            j++; k++;
+        }
+    }
+    while(i<=mid)
+    {
+        c[k]=a[i];
+        i++; k++;
+    }
+    while(j<=high)
+    {
+        c[k]=a[j];
+        j++; k++;
+    }
+    for(int i=low,j=0;i<=high;i++,j++)
+    {
+        a[i]=c[j];
+    }
+    delete[] c;
+}
+void merge_sort(int a[],int low,int high)
+{
+    if(low<high)
+    {
+        int mid=(low+high)/2;
+        merge_sort(a,low,mid);
+        merge_sort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+}
+
+//Quick Sort
+int partition(int a[],int low ,int high)
+{
+    int pivot=high;
+    int i=low-1;
+    for(int j=low;j<high;j++)
+    {
+        if(a[j]<a[pivot])
+        {
+            i++;
+            int t=a[j];
+            a[j]=a[i];
+            a[i]=t;
+        }
+    }
+    i++;
+    int t=a[i];
+    a[i]=a[pivot];
+    a[pivot]=t;
+    return i;
+}
+void quick_sort(int a[],int low ,int high)
+{
+    if(low<high)
+    {
+        int pi=partition(a,low,high);
+        quick_sort(a,low,pi-1);
+        quick_sort(a,pi+1,high);
+    }
 }
